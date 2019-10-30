c017af931c07a0d928f64bf9f276df044111acb5
Ciaran McGarry <ciaran.mcgarry@mastercard.com>
2019-10-30 17:09:00 +0000

    Initial setup

---
+++ src/main/java/EvolveSim/App.java
@@ -0,0 +1,13 @@
+package EvolveSim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
